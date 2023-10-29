cab8a8262e17c2eed87f2642b9b159e8580aeb1e
olirege <90354520+olirege@users.noreply.github.com>
2023-10-29 11:46:11 -0400

    Update config.ts

--- CropLink/src/firebase/config.ts
+++ CropLink/src/firebase/config.ts
@@ -1,9 +1,2 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAns9BWcC3bmgEXxLBOrTy57wqnbKqGPLQ",
-    authDomain: "croplink-30e3c.firebaseapp.com",
-    projectId: "croplink-30e3c",
-    storageBucket: "croplink-30e3c.appspot.com",
-    messagingSenderId: "25315620274",
-    appId: "1:25315620274:web:12e23c7db1827527b4a0ab",
-    measurementId: "G-2RDY6XGWPC"
 };
